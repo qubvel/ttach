82d9413179166e597badb91b6c245493bfbb62d5
Pavel Yakubovskiy <qubvel@gmail.com>
2020-10-13 19:38:17 +0300

    Update wrappers.py

--- ttach/wrappers.py
+++ ttach/wrappers.py
@@ -57,7 +57,7 @@ class ClassificationTTAWrapper(nn.Module):
             (.forward(x) should return either torch.Tensor or Mapping[str, torch.Tensor])
         transforms (ttach.Compose): composition of test time transforms
         merge_mode (str): method to merge augmented predictions mean/gmean/max/min/sum/tsharpen
-        output_mask_key (str): if model output is `dict`, specify which key belong to `label`
+        output_label_key (str): if model output is `dict`, specify which key belong to `label`
     """
 
     def __init__(
